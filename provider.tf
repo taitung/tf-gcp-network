d4b627658fa25b59c9fe1f822080e63beddb5194
taitung <guoguang@tresl.co>
2024-04-08 14:24:59 +0800

    Remove credentials

--- provider.tf
+++ provider.tf
@@ -2,5 +2,6 @@ provider "google" {
   project = var.gcp_project.project_id
   region = var.gcp_project.region 
   zone = var.gcp_project.zone
-  credentials = var.gcp_project.credentials
+  credentials = var.gcp_credentials
 }
+
--- variables.tf
+++ variables.tf
@@ -3,22 +3,24 @@ variable gcp_project {
     project_id = string
     region = string
     zone = string
-    credentials = string
   })
   default = {
     project_id = "dev1-417720"
     region = "asia-east1"
     zone = "asia-east1-a"
-    credentials = "tf-gcp-secret.json"
   }
 }
 
-variable gcp_auto_network_name {
+variable "gcp_credentials" {
+  type = string
+}
+
+variable "gcp_auto_network_name" {
   type = string
   default = "ggc-auto-network"
 }
 
-variable gcp_custom_network_name {
+variable "gcp_custom_network_name" {
   type = string
   default = "ggc-custom-network"
 }
